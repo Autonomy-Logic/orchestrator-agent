0095321fdfa0ec3fd20cbfc3ab4e2afeacc7e3b7
Thiago Alves <thiagoralves@gmail.com>
2025-11-13 10:02:59 -0500

    Remove self-signed certificate from server

--- src/tools/ssl.py
+++ src/tools/ssl.py
@@ -4,9 +4,9 @@ from aiohttp import ClientSession, TCPConnector
 
 client_cert = os.path.expanduser("~/.mtls/client.crt")
 client_key = os.path.expanduser("~/.mtls/client.key")
-server_ca_cert = os.path.expanduser("~/.mtls/ca.crt")
+#server_ca_cert = os.path.expanduser("~/.mtls/ca.crt")
 
-ssl_context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=server_ca_cert)
+ssl_context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH) #, cafile=server_ca_cert)
 ssl_context.minimum_version = ssl.TLSVersion.TLSv1_2
 ssl_context.load_cert_chain(certfile=client_cert, keyfile=client_key)
 
